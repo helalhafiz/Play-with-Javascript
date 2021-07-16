25b39058475c16c162fd5d271de28587cdf31f29
tafhimhasan <tafhimhasan99@gmail.com>
2021-07-16 22:43:13 +0600

    js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.dir('Hello Programmers!');
+
+
+//[menu_in_post_menu menu=37 style="dropdown" placeholder_text="Make A Payment"]j
+document.getElementById('frlinkOne').addEventListener('click',(e)=>window.location.href = 'https://pay.allianceassociationbank.com/Home?cmcid=3D476BCD');
+	
+	document.getElementById('frlinkTwo').addEventListener('click',(e)=>window.location.href = 'https://www.evanpandell.com/all-other-payments/');
